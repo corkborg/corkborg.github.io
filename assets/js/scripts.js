d0429c88877051c8b6e6a2d11ec0ce40c296a31b
corkborg <62742115+corkborg@users.noreply.github.com>
2026-08-18 00:29:18 +0900

    Publii: update content

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,14 @@
 // This is your main script file
+
+(() => {
+    const targets = [];
+    targets.push(...document.querySelectorAll(".post__image img"));
+    targets.push(...document.querySelectorAll("#featured-image img"));
+
+    for(const target of targets) {
+        target.style = "cursor: pointer;";
+        target.addEventListener("click", (e) => {
+            window.open(target.src);
+        });
+    }
+})();
